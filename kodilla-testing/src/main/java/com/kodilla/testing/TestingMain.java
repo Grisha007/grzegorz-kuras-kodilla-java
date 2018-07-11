9f14d6f61a40b970ce5d02905fd75d1f65ee8854
Grisha007 <k.greg@wp.pl>
2018-07-11 19:37:15 +0200

    Create test: user and calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,31 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser user1 = new SimpleUser("Name1");
+
+        String result = user1.getUsername();
+
+        if(result.equals("Name1")){
+            System.out.println("OK");
+        } else{
+            System.out.println("ERROR");
+        }
+
+        System.out.println("TEST - pierwszy test jednostkowy\n");
+
+        System.out.println("Calculator test");
+        System.out.println("==================\n");
+
+        Calculator calculator1 = new Calculator(9, 5);
+
+        int addResult = calculator1.add();
+        int subtractResult = calculator1.subtract();
+
+        System.out.println("Add test: " + addResult);
+        System.out.println("Subtract test: " + subtractResult);
     }
 }
