7dae15653d5ee39ca055ef805c76095578744892
Grisha007 <k.greg@wp.pl>
2018-07-27 19:57:25 +0200

    Zadanie: Niemutowalny użytkownik forum

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.*;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -14,15 +15,24 @@ public class StreamMain {
         expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
         expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
 
+        System.out.println();
+
         System.out.println("Calculating expressions with method references");
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
 
+        System.out.println();
+
         PoemBeautifier poemBeautifier = new PoemBeautifier();
         poemBeautifier.beautify("Test Test Test", (text) -> text.toUpperCase());
         poemBeautifier.beautify("Test Test Test", (text) -> "ABC " + text + " ABC");
         poemBeautifier.beautify("Test Test Test", (text) -> text.toLowerCase());
+
+        System.out.println();
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
