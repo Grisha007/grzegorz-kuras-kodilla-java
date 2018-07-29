7d6e8d9ddf239f5272a8dc5512e98d76febf78fa
Grisha007 <k.greg@wp.pl>
2018-07-29 13:12:21 +0200

    Zadanie: funkcyjny spacer po liście użytkowników forum + reszta z modułu 7.3. Poznanie funkcji stream(), filter(), map() i collect()

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,9 +1,19 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.book.Book;
+import com.kodilla.stream.book.BookDirectory;
+import com.kodilla.stream.forumuser.Forum;
+import com.kodilla.stream.forumuser.ForumUser;
 import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.*;
+import com.kodilla.stream.person.People;
 import com.kodilla.stream.reference.FunctionalCalculator;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -34,5 +44,70 @@ public class StreamMain {
 
         System.out.println("Using Stream to generate even numbers from 1 to 20");
         NumbersGenerator.generateEven(20);
+
+        System.out.println();
+
+        People.getList().stream()
+                .map(s -> s.toUpperCase())
+                .filter(s -> s.length() > 11)
+                .map(s -> s.substring(0, s.indexOf(' ') + 2) + ".")
+                .filter(s -> s.substring(0, 1).equals("M"))
+                .forEach(System.out::println);
+
+        System.out.println();
+
+        BookDirectory theBookDirectory = new BookDirectory();
+        theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .forEach(System.out::println);
+
+        System.out.println();
+
+        BookDirectory theBookDirectory1 = new BookDirectory();
+        List<Book> theResultListOfBooks = theBookDirectory1.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .collect(Collectors.toList());
+
+        System.out.println("# elements: " + theResultListOfBooks.size());
+        theResultListOfBooks.stream()
+                .forEach(System.out::println);
+
+        System.out.println();
+
+        BookDirectory theBookDirectory2 = new BookDirectory();
+        Map<String, Book> theResultMapOfBooks = theBookDirectory2.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .collect(Collectors.toMap(Book::getSignature, book -> book));
+
+        System.out.println("# elements: " + theResultMapOfBooks.size());
+        theResultMapOfBooks.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
+                .forEach(System.out::println);
+
+        System.out.println();
+
+        BookDirectory theBookDirectory3 = new BookDirectory();
+        String theResultStringOfBooks = theBookDirectory3.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .map(Book::toString)
+                .collect(Collectors.joining(",\n","<<",">>"));
+
+        System.out.println(theResultStringOfBooks);
+
+        System.out.println();
+
+        //LocalDate today = LocalDate.now();
+
+        Forum forum = new Forum();
+        Map<Integer, ForumUser> theResultMapOfForum = forum.getUsersList().stream()
+                .filter(forumUser -> forumUser.getSex() == 'M')
+                .filter(forumUser -> LocalDate.now().compareTo(forumUser.getBirthDate()) >= 20)
+                .filter(forumUser -> forumUser.getNumberOfPosts() >= 1)
+                .collect(Collectors.toMap(ForumUser::getUserId, forumUser -> forumUser));
+
+        System.out.println("# elements: " + theResultMapOfForum.size());
+        theResultMapOfForum.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
+                .forEach(System.out::println);
     }
 }
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/forumuser/Forum.java
@@ -0,0 +1,23 @@
+package com.kodilla.stream.forumuser;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Forum {
+    private final List<ForumUser> usersList = new ArrayList<>();
+
+    public Forum(){
+        usersList.add(new ForumUser(78945, "Username01", 'M', LocalDate.of(1987,6,15), 152));
+        usersList.add(new ForumUser(45621, "Username02", 'F', LocalDate.of(1997,5,10), 0));
+        usersList.add(new ForumUser(23658, "Username03", 'M', LocalDate.of(1995,2,11), 116));
+        usersList.add(new ForumUser(74359, "Username04", 'F', LocalDate.of(2007,5,20), 75));
+        usersList.add(new ForumUser(15874, "Username05", 'F', LocalDate.of(1998,10,25), 0));
+        usersList.add(new ForumUser(13492, "Username06", 'M', LocalDate.of(2002,9,16), 15));
+        usersList.add(new ForumUser(52873, "Username07", 'M', LocalDate.of(1982,4,7), 128));
+    }
+
+    public List<ForumUser> getUsersList(){
+        return new ArrayList<>(usersList);
+    }
+}
