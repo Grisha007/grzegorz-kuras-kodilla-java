6cc045f32934e10ed872a2ae2b7ed98ed20c33ee
Grisha007 <k.greg@wp.pl>
2018-08-05 09:14:24 +0200

    Zadanie: Wiedza o wyjątkach

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearch.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearch.java
@@ -11,9 +11,9 @@ public class FlightSearch {
         airports.put("London Airport", true);
 
         for(Map.Entry<String, Boolean> entry :airports.entrySet()) {
-            if (flight.getArrivalAirport().equals(entry.getKey()) && entry.getValue() == true) {
+            if (flight.getArrivalAirport().equals(entry.getKey()) && (entry.getValue() == true)) {
                 System.out.println("Flight status: OK!");
-            } else if (flight.getArrivalAirport().equals(entry.getKey()) && entry.getValue() == false) {
+            } else if (flight.getArrivalAirport().equals(entry.getKey()) && (entry.getValue() == false)) {
                 System.out.println("Arrival airoprt is CLOSE!");
             } else{
                 throw new RouteNotFoundException("Airport is not available in database!");
