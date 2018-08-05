79b202eb9d7008fb68988baa98372c2758f9851f
Grisha007 <k.greg@wp.pl>
2018-08-05 11:37:42 +0200

    Zadanie: Wiedza o wyjątkach - corrected

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightRunner.java
@@ -4,17 +4,16 @@ import java.util.Map;
 
 public class FlightRunner {
     public static void main(String[] args) {
-        Flight flight1 = new Flight("Wroclaw Airport", "Tokyo Airport");
+        Flight flight1 = new Flight("Wroclaw Airport", "Budapes Airport");
 
         FlightSearch flightSearcher = new FlightSearch();
 
         try {
-            Map<String, Boolean> flightStatus = flightSearcher.findFilght(flight1);
-            System.out.println(flightStatus);
+            flightSearcher.findFilght(flight1);
         } catch(RouteNotFoundException e){
             System.out.println("Error occured! Error status: " + e);
         } finally{
-            System.out.println("Please check your arrival airport!");
+            System.out.println("Have a nice day!");
         }
     }
 }
--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearch.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearch.java
@@ -4,21 +4,23 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FlightSearch {
-    public Map<String, Boolean> findFilght(Flight flight) throws RouteNotFoundException{
+    public void findFilght(Flight flight) throws RouteNotFoundException {
         Map<String, Boolean> airports = new HashMap<>();
         airports.put("Wroclaw Airport", true);
         airports.put("Tokyo Airport", false);
         airports.put("London Airport", true);
 
-        for(Map.Entry<String, Boolean> entry :airports.entrySet()) {
-            if (flight.getArrivalAirport().equals(entry.getKey()) && (entry.getValue() == true)) {
+        if (airports.containsKey(flight.getArrivalAirport())) {
+
+            boolean status = airports.get(flight.getArrivalAirport());
+
+            if (status == true) {
                 System.out.println("Flight status: OK!");
-            } else if (flight.getArrivalAirport().equals(entry.getKey()) && (entry.getValue() == false)) {
-                System.out.println("Arrival airoprt is CLOSE!");
-            } else{
-                throw new RouteNotFoundException("Airport is not available in database!");
+            } else {
+                System.out.println("Arrival airport is CLOSE!");
             }
+        } else {
+            throw new RouteNotFoundException("Airport is not available in database!");
         }
-        return airports;
     }
 }
